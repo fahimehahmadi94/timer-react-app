9d365167970e30b16e9ee5cf1bec6eb362fc859e
fahimehahmadi <fahimehahmadi94@gmail.com>
2022-02-09 21:53:14 +0330

    part8

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//part 7
+// const tick = () => {
+//   const elem = (
+//     <div>
+//       <h1>Hi everyone</h1>
+//       <h2>It is {new Date().toLocaleString() } </h2>
+//     </div>
+//   );
+//   ReactDOM.render(elem, document.getElementById("root"));
+// };
 
+// setInterval(() => {
+//   tick();
+// }, 1000);
+//part 8
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello/>
+       <Timer/>
+      </div>
+    )
+  }
+}
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1> Hi everyone</h1>
+    )
+  }
+}
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>It is {new Date().toLocaleDateString()}</h2>
+    )
+  }
+}
 const tick = () => {
-  const elem = (
-    <div>
-      <h1>Hi everyone</h1>
-      <h2>It is {new Date().toLocaleString() } </h2>
-    </div>
-  );
-  ReactDOM.render(elem, document.getElementById("root"));
+  ReactDOM.render(<App/>, document.getElementById("root"));
 };
 
 setInterval(() => {
