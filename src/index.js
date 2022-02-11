488c54e3ddb10dff2a3e32b8e447316f5eed6411
fahimehahmadi <fahimehahmadi94@gmail.com>
2022-02-11 16:16:36 +0330

    part9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 //part 7
 // const tick = () => {
 //   const elem = (
@@ -16,34 +17,70 @@ import ReactDOM from "react-dom";
 // }, 1000);
 //part 8
 
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Hello/>
+//        <Timer />
+//       </div>
+//     )
+//   }
+// }
+// class Hello extends React.Component{
+//   render(){
+//     return(
+//       <h1> Hi everyone</h1>
+//     )
+//   }
+// }
+// class Timer extends React.Component{
+//   render(){
+//     return(
+//       <h2 className="timer">It is {new Date().toLocaleTimeString()}</h2>
+//     )
+//   }
+// }
+// const tick = () => {
+//   ReactDOM.render(<App/>, document.getElementById("root"));
+// };
+
+// setInterval(() => {
+//   tick();
+// }, 1000);
+//part9
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Hello/>
-       <Timer/>
+      <div className="main">
+        <Hello />
+        <Timer />
       </div>
-    )
+    );
   }
 }
-class Hello extends React.Component{
-  render(){
-    return(
-      <h1> Hi everyone</h1>
-    )
+class Hello extends React.Component {
+  render() {
+    return <h1> Hi everyone</h1>;
   }
 }
-class Timer extends React.Component{
-  render(){
-    return(
-      <h2>It is {new Date().toLocaleDateString()}</h2>
-    )
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
   }
-}
-const tick = () => {
-  ReactDOM.render(<App/>, document.getElementById("root"));
-};
 
-setInterval(() => {
-  tick();
-}, 1000);
+  render() {
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+
+    return <h2 className="timer">It is {this.state.time}</h2>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
