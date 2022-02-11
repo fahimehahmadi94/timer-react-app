2f9a1958852280072bcebb1ef795d862013c679e
fahimehahmadi <fahimehahmadi94@gmail.com>
2022-02-11 17:43:30 +0330

    part11

--- src/Timer.js
+++ src/Timer.js
@@ -2,23 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+let interval;
+
 class Timer extends React.Component {
-    constructor() {
-      super();
-      this.state = {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+  componentDidMount() {
+    interval = setInterval(() => {
+      this.setState({
         time: new Date().toLocaleTimeString(),
-      };
-    }
-  
-    render() {
-      setInterval(() => {
-        this.setState({
-          time: new Date().toLocaleTimeString(),
-        });
-      }, 1000);
-  
-      return <h2 className="timer">It is {this.state.time}</h2>;
-    }
+      });
+    }, 1000);
   }
 
-  export default Timer;
+  componentDidUpdate() {
+    if (this.state.time=="5:43:15 PM") {
+      clearInterval(interval);
+    }
+  }
+  componentWillUnmount() {}
+  render() {
+    return <h2 className="timer">It is {this.state.time}</h2>;
+  }
+}
+
+export default Timer;
