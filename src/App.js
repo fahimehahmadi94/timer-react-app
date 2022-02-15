d48e1bde876fe302993a339803b052b8b507761b
fahimehahmadi <fahimehahmadi94@gmail.com>
2022-02-16 00:57:40 +0330

    part17

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import Timer from "./Timer";
 import Hello from "./Hello";
+import TimeList from "./TimeList";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "HI",
-    };
-  }
-  handlelSetTitle = () => {
-    this.setState({
-      title: "خوش آمدید",
-    });
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       title: "HI",
+//     };
+//   }
+//   handlelSetTitle = () => {
+//     this.setState({
+//       title: "خوش آمدید",
+//     });
+//   };
+//   render() {
+//     return (
+//       <div className="main">
+//         <Hello title={this.state.title} />
+//         <Timer handlelSetTitle={this.handlelSetTitle} />
+//       </div>
+//     );
+//   }
+// }
+const App = () => {
+  const [title, setTitle] = useState("salam dostam");
+  const [isLight, setIsLight] = useState(false);
+  const [timeArr, setTimeArr] = useState(["00:05:12"]);
+
+  useEffect(() => {
+    console.log("useEfect");
+    return () => {};
+  }, [isLight]);
+
+  const handlelSetIsLight = () => {
+    setIsLight(!isLight);
   };
-  render() {
-    return (
-      <div className="main">
-        <Hello title={this.state.title} />
-        <Timer handlelSetTitle={this.handlelSetTitle} />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="main" style={{ background: isLight ? "white" : "black" }}>
+      <Hello title={title} />
+      <Timer isLight={isLight} handlelSetIsLight={handlelSetIsLight} />
+      <TimeList>{timeArr}</TimeList>
+    </div>
+  );
+};
 export default App;
